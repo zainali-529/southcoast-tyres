0dbb1daf6396dd6dfbdb64b8bac6fe2274b82b93
themarketinglab10-boop <themarketinglab10@gmail.com>
2026-03-03 14:38:45 +0500

    Update AboutUs.jsx

--- src/pages/landingpage/components/AboutUs.jsx
+++ src/pages/landingpage/components/AboutUs.jsx
@@ -25,8 +25,10 @@ const AboutUs = () => {
           <div className="text-container ms-md-4" data-aos="fade-left">
             <h2>About Us</h2>
             <h1 className="mb-3">Dedicated to Keeping You Safe on the Road</h1>
-            <p>
-              At 24/7 South Coast Mobile Tyre, we specialize in providing top-notch tire services to ensure your safety and comfort on the road. With years of experience, our team is committed to offering reliable and efficient solutions for all your tire needs.
+            <p> 
+              At South Coast Mobile Tyres, we combine the massive inventory of a professional tyre shop with the ultimate convenience of a 24/7 mobile service. Whether you are looking for specific premium new tyres or need an emergency roadside fix, we are dedicated to keeping you safe on the road.
+
+We provide rapid-response mobile tyre fitting, tyre replacement, and puncture repairs directly to your home, workplace, or the roadside. With a 30 minute arrival time, we ensure you are never stranded for long. we specialize in providing top-notch tire services to ensure your safety and comfort on the road. With years of experience, our team is committed to offering reliable and efficient solutions for all your tire needs.
             </p>
             <button className="btn" data-aos="zoom-in" onClick={handleScrollToServices}>
               Visit Services
